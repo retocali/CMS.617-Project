e9cd5bb856328de2dbdc25f40b051116cbe5ae12
Arthur Williams <taaparthur@gmail.com>
2018-09-16 12:45:15 -0400

    Added moving platforms
    The velocity, acceleration and turnaround delay are all tunable
    parameters. The platform can start at an arbitrary offset into its cycle
    also.
    
    Created a scene where the player is riding a vertical moving platform.
    
    NOTE: that if the player is on the platform and the platform is moving
    horizontally then the player will fall off if he doesn't match the x
    velocity

---
+++ SLIME/Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private Vector3 normalScale;
+	
+	public float xAcceleration = 0;
+	public float yAcceleration = 0;
+	public float yVelocity=10;
+	public float xVelocity=0;
+	///after cycles frames the direction switches
+        public uint cycles = 100;
+	public uint cycleOffset= 50;
+	///number of frames to wait after completing a cycle to switching direction
+	public uint turnAroundDelay=100;
+	private uint delayCounter=0;
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D>();
+		resetVelocity();
+		normalScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(delayCounter!=0){
+			delayCounter--;
+			if(delayCounter==0)
+				resetVelocity();
+			return;
+		}
+		cycleOffset++;
+		if(cycleOffset%cycles ==0){
+			yVelocity=-yVelocity;
+			xVelocity=-xVelocity;
+			xAcceleration=-xAcceleration;
+			yAcceleration=-yAcceleration;
+			if(turnAroundDelay>0)
+				rb.velocity = new Vector2(0,0);
+			else resetVelocity();
+			delayCounter=turnAroundDelay;
+			return;
+		}
+		Debug.Log(rb.velocity);
+		Debug.Log(rb.position);
+		float deltaT=Time.deltaTime;
+		rb.velocity += new Vector2(deltaT*xAcceleration, deltaT*yAcceleration);
+	}
+	void resetVelocity(){
+		///should save this value so we don't have to recreate it
+		rb.velocity = new Vector2(xVelocity,yVelocity);
+	}
+}
