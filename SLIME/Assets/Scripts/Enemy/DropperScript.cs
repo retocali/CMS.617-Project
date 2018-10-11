dd6cfb893ef5516879100226f58a09e4458f270e
Rodrigo T <retocali@mit.edu>
2018-10-11 14:17:09 -0400

    Added folder and initial spawner&dropper

---
+++ SLIME/Assets/Scripts/Enemy/DropperScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class DropperScript : MonoBehaviour {
+	public bool sensitive = true;
+	
+	private Vector3 velocity;
+	private Vector2 addedVelocity;
+	private Controller2D c2d;
+	private float gravity = -20f;
+	private bool falling = false;
+	private Transform player;
+
+	private float XRange = 1;
+	private float YRange = 1;
+
+	// Use this for initialization
+	void Start () {
+		velocity = Vector3.zero;
+		c2d  = GetComponent<Controller2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!PlayerInRange() && !falling)
+		{
+			return;
+		}
+
+		falling = true;
+		
+		velocity.y += addedVelocity.y;
+		velocity.x += addedVelocity.x;
+		addedVelocity = Vector2.zero;
+
+		velocity.y += Time.deltaTime*gravity;
+		if (c2d.collision.below) {
+			Destroy(gameObject, 0.25f);
+		}
+		if (c2d.collision.above) {
+			velocity.y = 0;
+		}
+		if (c2d.collision.right || c2d.collision.left) {
+			velocity.x = 0;
+		}
+
+		c2d.Move(velocity*Time.deltaTime);
+	}
+
+	private bool PlayerInRange()
+	{
+		if (!sensitive) { return true; }
+		if (player == null)
+		{
+			player = PlayerScript.FindPlayer().transform;
+		}
+
+		return (Mathf.Abs(player.position.x-transform.position.x) < XRange
+					  && (player.position.y-transform.position.y) < YRange);
+	}
+
+	public void AddVelocity(Vector2 v)
+	{
+		addedVelocity += v;
+	}
+}
---
+++ SLIME/Assets/Scripts/Enemy/SpawnerScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+
+	public GameObject spawnPrefab;
+	
+	public bool sensitive = false;
+	public float time = 2f;
+	public int number = 10;
+	
+	private float currentTime = 0;
+	private float speed = 15f;
+	private Transform player;
+	private MeshRenderer mesh;
+
+	private float XRange = 5;
+	private float YRange = 5;
+	// Use this for initialization
+	void Start () {
+		if (spawnPrefab == null)
+		{
+			spawnPrefab = gameObject;
+		}
+
+		mesh = GetComponent<MeshRenderer>();
+	}
+	// Update is called once per frame
+	void Update () {
+		if (sensitive && PlayerInRange())
+		{
+			Debug.Log(time-currentTime);
+			currentTime += Time.deltaTime;
+			mesh.material.color = new Color(1-currentTime/time, 0, 0);
+		}
+		
+		if (currentTime >= time) {
+			currentTime = 0;
+			mesh.material.color = Color.black;
+			Spawn();
+		}
+	}
+
+	private bool PlayerInRange()
+	{
+		if (!sensitive) { return true; }
+		if (player == null)
+		{
+			player = PlayerScript.FindPlayer().transform;
+		}
+
+		return (Mathf.Abs(player.position.x-transform.position.x) < XRange
+		     && Mathf.Abs(player.position.y-transform.position.y) < YRange);
+	}
+	private void Spawn()
+	{
+		float step = (Mathf.PI/number);
+		for (float rad = Mathf.PI/4; rad < 3*Mathf.PI/4; rad += step)
+		{
+			Vector3 displacement = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0);
+			var s = (GameObject) Instantiate(spawnPrefab,
+											 this.transform.position,
+											 this.transform.rotation);
+			s.GetComponent<DropperScript>().AddVelocity(speed*displacement);
+		}
+	
+	}
+}
