9695d860c51e13fa58363c2c2d0234c3981e1ed9
Rodrigo T <retocali@mit.edu>
2018-10-11 15:18:24 -0400

    Added enemy interface and last enemy

---
+++ SLIME/Assets/Scripts/Enemy/EnemyClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface EnemyClass {
+	
+	void Respawn();
+	void KillEnemy();
+
+	void AddVelocity(Vector2 v);
+	void MultiplyVelocity(float f);
+	void ApplyGravity(ref Vector3 velocity, float gravity);
+}
