c6bb55cf9714058a96349aeb3390846d35dd58bc
Christopher Madrigal <cmadriga@mit.edu>
2018-11-19 00:56:48 -0500

    rudimentary shooting AI and new final boss urgency model added that uses this AI

---
+++ SLIME/Assets/Scripts/Enemy/FinalBossScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossScript : MonoBehaviour {
+
+public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	private float lastShot;
+
+	public float fireRate;
+
+	public float fireSpeed;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		lastShot = -fireRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > lastShot + (fireRate )){
+			lastShot = Time.time;
+			Fire();
+		}
+	}
+	
+
+	void Fire(){
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate (
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+			Vector3 direction = (player.transform.position - transform.position).normalized * fireSpeed;
+
+			// Add velocity to the bullet
+			bullet.GetComponent<Rigidbody2D>().velocity = direction;
+
+			// bullet.GetComponent<FireBallScript>().SetPlayerPos(player);
+			// bullet.GetComponent<FireBallScript>().SetSpeed(fireSpeed);
+
+			Destroy(bullet, 5f);
+	}
+}
---
+++ SLIME/Assets/Scripts/Enemy/FireBallScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallScript : MonoBehaviour {
+
+	private GameObject player;
+
+	private float fireSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+        // Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+ 
+        // // Aim bullet in player's direction.
+        // transform.rotation = Quaternion.LookRotation(playerPos);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 // Move the projectile forward towards the player's last known direction;
+        // transform.position += transform.forward * fireSpeed;
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.GetComponent<PlayerScript>().KillPlayer();
+			Destroy(gameObject);
+		}
+	}
+
+	// public void SetPlayerPos(GameObject p) {
+	// 	player = p;
+	// }
+
+	// public void SetSpeed(float s) {
+	// 	fireSpeed = s;
+	// }
+}
